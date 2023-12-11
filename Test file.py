c4779dba4da8aad8a9d8891bdc7db5abe9dff143
Amirsamandar <90907234+Amirsamandar@users.noreply.github.com>
2023-12-11 00:11:56 -0500

    beta

--- Test file.py
+++ Test file.py
@@ -1,11 +1,28 @@
 from solar_system import DynamicalSystem as ds
 import matplotlib.pyplot as plt
 import numpy as np
-from solar_system import NBodies
+from solar_system import CelestialBody
 
 solar = ds(400)
-body = NBodies(solar,100,velocity=np.array([1,1,1]))
 
-for _ in range(100):
+planets = (CelestialBody(
+        solar,
+        mass = 10_000 ,color ="yellow",
+    ),
+    CelestialBody(
+        solar,
+        position=np.array([150, 50, 0]),
+        velocity=np.array([0, 5, 5]),color ="red"
+    ),
+    CelestialBody(
+        solar,
+        mass=20,
+        position=np.array([100, -50, 150]),
+        velocity=np.array([5, 0, 0]), color ="blue"
+    )
+)
+
+while True:
+    solar.dynamical_interaction()
     solar.update_all()
     solar.display_all()
--- solar_system.py
+++ solar_system.py
@@ -2,73 +2,81 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class DynamicalSystem:
-    
     def __init__(self, size):
-        
         self.size = size
         self.bodies = []
 
-        self.fig , self.ax = plt.subplots(1,1, subplot_kw={"projection" : "3d"}, figsize = (self.size/50 , self.size/50))
+        self.fig, self.ax = plt.subplots(1, 1, subplot_kw={"projection": "3d"}, figsize=(self.size / 50, self.size / 50))
         self.fig.tight_layout()
+        self.ax.view_init(0, 0)
 
     def add_body(self, body):
-       self.bodies.append(body)
+        self.bodies.append(body)
 
-    
     def update_all(self):
         for body in self.bodies:
             body.motion()
-
-    def display_all(self):
-        for body in self.bodies:
             body.display()
-             
-       
-   
 
+    def display_all(self): 
+        self.ax.set_xlim((-self.size / 2, self.size / 2))
+        self.ax.set_ylim((-self.size / 2, self.size / 2))
+        self.ax.set_zlim((-self.size / 2, self.size / 2))
+        plt.pause(0.01)
+        self.ax.clear()
+
+    def dynamical_interaction(self):
+        for index, body in enumerate(self.bodies):
+            for other in self.bodies[index + 1:]:
+                body.gravitational_interaction(other)
 
 
 class NBodies:
-
-    def __init__(self,dynamics, mass , position = np.array([0,0,0]), velocity=np.array([0,0,0])):
-
-        self.dynamics = dynamics
+    def __init__(self, dynamical_box, mass, position=np.array([0., 0., 0.]), velocity=np.array([0., 0., 0.])):
+        self.dynamical_box = dynamical_box
         self.mass = mass
-        self.position = position
-        self.velocity = velocity
-        self.dynamics.add_body(self)
+        self.position = np.array(position)
+        self.velocity = np.array(velocity)
+        self.dynamical_box.add_body(self)
 
-        self.display_size = max( np.log2(self.mass) , 10 )
-
+        self.display_size = int(max(np.log2(self.mass), 10))
         self.color = "black"
 
     def motion(self):
-
-        self.position = (
-            self.position[0]+self.velocity[0],
-            self.position[1]+self.velocity[1],
-            self.position[2]+self.velocity[2])
-        
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+        self.position[2] += self.velocity[2]
 
     def display(self):
-        self.dynamics.ax.plot(*self.position,marker="o", markersize=self.display_size , color = self.color)
-        self.dynamics.ax.set_xlim((-self.dynamics.size/2,self.dynamics.size/2))
-        self.dynamics.ax.set_ylim((-self.dynamics.size/2,self.dynamics.size/2))
-        self.dynamics.ax.set_zlim((-self.dynamics.size/2,self.dynamics.size/2))
-        plt.pause(0.001)
-        self.dynamics.ax.clear()
-      
-
-
-
-   
+        self.dynamical_box.ax.plot(*self.position, marker="o", markersize=self.display_size, color=self.color)
         
+     
 
-   
-   
-       
+    def gravitational_interaction(self, other):
+        G = 1.
+        distance_vec = np.array(self.position - other.position)
+        distance_mag = np.sqrt(np.sum(distance_vec ** 2))
+        gravity_force = G * self.mass * other.mass / distance_mag** 2
+        self.velocity[0] += (gravity_force / (distance_mag * self.mass)) * distance_vec[0]
+        self.velocity[1] += (gravity_force / (distance_mag * self.mass)) * distance_vec[1]
+        self.velocity[2] += (gravity_force / (distance_mag * self.mass)) * distance_vec[2]
+        other.velocity[0] += -(gravity_force / (distance_mag * other.mass)) * distance_vec[0]
+        other.velocity[1] += -(gravity_force / (distance_mag * other.mass)) * distance_vec[1]
+        other.velocity[2] += -(gravity_force / (distance_mag * other.mass)) * distance_vec[2]
 
 
 
 
-   
+
+
+
+class CelestialBody(NBodies): 
+    def __init__(self, dynamical_box, mass=10, position=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]), color="black"):
+        super(CelestialBody, self).__init__(dynamical_box, mass, position, velocity)
+        self.color = color
+
+
+
+
+
+
