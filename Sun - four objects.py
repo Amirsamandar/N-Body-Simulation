f9fbbeb68f9fb64fbf2be4ad1979347bf03c69f0
Amirsamandar <90907234+Amirsamandar@users.noreply.github.com>
2023-12-18 11:17:43 -0500

    the last last stand

--- Sun - four objects.py
+++ Sun - four objects.py
@@ -7,7 +7,7 @@ system = DynamicalSystem(size=400, projection2D=True , view = (10,0))
 sun = CelestialBody(system, mass=1000, position=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]), color="yellow")
 
 # Planets
-mercury = CelestialBody(system, mass=1, position=np.array([30.0, 0.0, 0.0]), velocity=np.array([0.0, 1.3, 0.0]), color="grey")
+# mercury = CelestialBody(system, mass=1, position=np.array([30.0, 0.0, 0.0]), velocity=np.array([0.0, 1.3, 0.0]), color="grey")
 venus = CelestialBody(system, mass=3, position=np.array([50.0, 0.0, 0.0]), velocity=np.array([0.0, 2.5, 0.0]), color="orange")
 earth = CelestialBody(system, mass=5, position=np.array([80.0, 0.0, 0.0]), velocity=np.array([0.0, 2.0, 0.0]), color="blue")
 mars = CelestialBody(system, mass=2, position=np.array([110.0, 0.0, 0.0]), velocity=np.array([0.0, 1.5, 0.0]), color="red")
